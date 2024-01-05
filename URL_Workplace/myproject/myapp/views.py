6332927bcd448d3b860e81450a4d050210d168d3
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-01-05 20:16:59 +0000

    Return a HTML response using django

--- URL_Workplace/myproject/myapp/views.py
+++ URL_Workplace/myproject/myapp/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     path = request.path
-    return HttpResponse(path, content_type = "text/html", charset="UTF-8")
+    response = HttpResponse("This works!")
+    return response
