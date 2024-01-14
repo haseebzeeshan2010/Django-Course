f8888a367a5baa325d56dd0c4bb71179a9742af4
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-01-14 20:17:41 +0000

    Create a model and a table inside of it

---
+++ models_example_project/menuproject/menuapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class Menu(models.Model): #In general one model is used per table
+    name = models.CharField(max_length = 100) #create a table like this
+    cuisine = models.CharField(max_length = 100) #charfield is used for strings
+    price =  models.IntegerField
