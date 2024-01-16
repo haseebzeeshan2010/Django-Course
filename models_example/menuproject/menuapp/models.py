c0aa9733ea8c12d77c9e9df9791c74f118c4547c
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-01-16 19:31:45 +0000

    Create project on managing entries

---
+++ models_example/menuproject/menuapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Menu(models.Model): #In general one model is used per table
+    name = models.CharField(max_length = 100) #create a table like this
+    cuisine = models.CharField(max_length = 100) #charfield is used for strings
+    price =  models.IntegerField()
+
+    def __str__(self):
+        return self.name + " : " + self.cuisine
