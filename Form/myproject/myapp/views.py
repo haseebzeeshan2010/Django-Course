91a271c80a40554a2b2acbcfe5cc4366568e16ac
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-02-11 22:05:41 +0000

    Create model for dynamic form

---
+++ Form/myproject/myapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from myapp.forms import InputForm
+
+# Create your views here.
+def form_view(request):
+    form = InputForm()
+    context = {"form": form}
+    return render(request, "home.html", context)
