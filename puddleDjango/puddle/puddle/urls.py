adf112f8eba350f7dfbb25be3ee4613ba8339efb
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-02-23 09:27:27 +0000

    Debugged and fixed items separate page

--- puddleDjango/puddle/puddle/urls.py
+++ puddleDjango/puddle/puddle/urls.py
@@ -1,13 +1,15 @@
 from django.conf import settings
 from django.conf.urls.static import static
+# Part above for handling the images(do not use in production)
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include 
 
 from core.views import index, contact
 
 urlpatterns = [
     path('', index, name='index'),
+    path('items/', include('item.urls')),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
