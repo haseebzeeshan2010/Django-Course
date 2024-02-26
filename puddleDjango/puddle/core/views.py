73556c0222ab24ae267bd4e48e6ed6076102625b
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-02-26 20:27:23 +0000

    Link the sign up page with rest of project

--- puddleDjango/puddle/core/views.py
+++ puddleDjango/puddle/core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from item.models import Category, Item # import models for showing items on homepage
 
+from .forms import SignupForm
+
 # Create your views here.
 
 def index(request):
@@ -14,3 +16,10 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+def signup(request):
+    form = SignupForm()
+
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
