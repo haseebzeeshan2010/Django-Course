b5da0833488a7e1881c1a5b6427237d797f9ab50
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-03-06 19:19:50 +0000

    Add detail to serach in categories

--- puddleDjango/puddle/item/views.py
+++ puddleDjango/puddle/item/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import NewItemForm, EditItemForm
@@ -9,7 +10,8 @@ def items(request):
     items = Item.objects.filter(is_sold=False)
 
     if query:
-        items = items.filter(name__icontains=query)
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
     return render(request, 'item/items.html', {
         'items': items,
         'query': query,
